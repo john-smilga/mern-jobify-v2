d54c33e9bebe2c1075399d6c13bf46ca09e4cc96
john smilga <johnsmilga@gmail.com>
2023-07-15 12:04:42 -0400

    update readme

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,8 +1,13 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/uploads');
+    cb(null, path.resolve(__dirname, '../public/uploads'));
   },
   filename: (req, file, cb) => {
     const fileName = file.originalname;
