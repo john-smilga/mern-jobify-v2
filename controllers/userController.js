3ac7a78cf98c73442f84c820646375aa8bbd0732
john smilga <johnsmilga@gmail.com>
2023-07-15 13:38:17 -0400

    upload image with buffer

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -17,10 +17,9 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
-  console.log(req.file);
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
@@ -29,6 +28,5 @@ export const updateUser = async (req, res) => {
   if (req.file && updatedUser.avatarPublicId) {
     await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
   }
-
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,15 +1,14 @@
 import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
+import { log } from 'console';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads');
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname;
-    cb(null, fileName);
-  },
-});
+const storage = multer.memoryStorage();
+export const upload = multer({ storage });
 
-const upload = multer({ storage });
+const parser = new DataParser();
 
-export default upload;
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
