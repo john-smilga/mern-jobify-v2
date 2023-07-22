b39f5cb6077661418efbf970fa52546b9540f045
john smilga <johnsmilga@gmail.com>
2023-07-22 12:57:10 -0400

    increase rate limit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -10,7 +10,7 @@ import rateLimiter from 'express-rate-limit';
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 15,
+  max: 20,
   message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
 });
 
